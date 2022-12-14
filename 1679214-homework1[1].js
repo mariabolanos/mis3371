be2bac26da970db9fbbb5e3271523eb781be1664
Maria Angelica Bolanos <65629983+mariabolanos@users.noreply.github.com>
2022-12-13 23:14:18 -0600

    Add files via upload

---
+++ 1679214-homework1[1].js
@@ -0,0 +1,19 @@
+PseudoCode:
+
+- made the structure of html tag structure with the DocType tag, meta tag the head tag, the body tag and the footer tag
+- created an additional document to write CSS in and linked it to the html sheet
+- added a script tag at the end 
+- Use tags to create the header of the page 
+- Used javascript and html to add the current date
+- Inside the body tag, used a fieldset tag to add a border around the form 
+- Used a table structure, label, input, and break tags, 
+- added the input boxes for the different inputs like First Name, Middle Initial and last name
+- added the minimum and maximum length requirements for the input boxes
+- added the required attribute to the input boxes that were mandatory for the user
+- using select tag, created a drop down for the user to select a state including the territories
+- added a checkbox using the checkbox type in a table structure with a blue background color
+- added radio buttons for the user to answer specific questions 
+-added footer and address using the header tags 
+- then added a link for a contact us page 
+- In script tag, add javascript 
+
